b3017491d300da64ed0719e4ccbe9b57610204fa
Isabelly Oliveira <isabelly.r060408@gmail.com>
2026-02-27 16:20:21 -0300

    metodo exibir detalhes

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 class ItemBiblioteca:
-    def __init__(self, __codigo, __tilulo, __ano, __disponivel):
-        self.__codigo = __codigo
+    def __init__(self, codigo: int, titulo: str, ano: int, disponivel: bool):
+        self.__codigo = codigo
         self.__titulo = titulo
-        self.__ano = __ano
-        self.__disponivel = __disponivel
+        self.__ano = ano
+        self.__disponivel = disponivel
+
+    def exibir_detalhes(self):
+        print(f"Código: {self.__codigo} - Título: {self.__titulo} - Ano: {self.__ano} Disponivel: {self.__disponivel}")
+        
+        
+detalhes = ItemBiblioteca(1, "pequeno principe", 1943, "sim")
+detalhes.exibir_detalhes()
