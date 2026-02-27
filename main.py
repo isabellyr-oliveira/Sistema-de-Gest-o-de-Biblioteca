8580e69240bd4497babc61c02855307492d156e1
Isabelly Oliveira <isabelly.r060408@gmail.com>
2026-02-27 15:51:47 -0300

    codigo inicial

---
+++ main.py
@@ -0,0 +1,6 @@
+class ItemBiblioteca:
+    def __init__(self, __codigo, __tilulo, __ano, __disponivel):
+        self.__codigo = __codigo
+        self.__titulo = titulo
+        self.__ano = __ano
+        self.__disponivel = __disponivel
